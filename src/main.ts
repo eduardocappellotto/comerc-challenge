3b4596092690df5cc48ba8b9a7497a237daf3543
Eduardo Cerioni Cappellotto <eduardocappellotto@gmail.com>
2024-02-24 12:58:54 -0300

    feat: :sparkles: registered components and layouts globally

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { registerComponents } from './components/register'
+import { registerLayouts } from './layouts/register'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+await registerLayouts(app)
+await registerComponents(app)
 
 app.mount('#app')
