00e9e5fda27bc8607568bff856ad1595ec007e6b
Eduardo Cerioni Cappellotto <eduardocappellotto@gmail.com>
2024-02-25 11:49:59 -0300

    chore: :lipstick: added colors and icons

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+        <CHeader />
+
+        <main class="flex-grow h-full">
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+  
--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,21 @@ import router from './router'
 
 import { registerComponents } from './components/register'
 import { registerLayouts } from './layouts/register'
+import { registerViews } from './views/register'
+
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import * as RiIcons from 'oh-vue-icons/icons/ri'
+
+const Ri = Object.values({ ...RiIcons })
+addIcons(...Ri)
 
 const app = createApp(App)
 
+app.component('v-icon', OhVueIcon)
 app.use(createPinia())
 app.use(router)
 await registerLayouts(app)
 await registerComponents(app)
+await registerViews(app)
 
 app.mount('#app')
